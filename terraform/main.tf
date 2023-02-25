6ce0a964f74cd39fcd41f1483c84c990de8e8271
a-sotnikov <sotnikov.andrey07@gmail.com>
2023-02-25 19:22:01 +0300

    added ansible and tf templates

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+# Ubuntu img = "fd8emvfmfoaordspe1jr"
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "name"
+  hostname = "hostname"
+
+  resources {
+    cores  = 2
+    core_fraction = 20
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8emvfmfoaordspe1jr"
+      size = 20
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.100.0/24"]
+
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
